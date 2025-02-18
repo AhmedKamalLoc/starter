0cb0a9c3baafb66df859e41901dcdd667e4b14a8
AhmedRagabKamal <ahmed.ragab.kamal@gmail.com>
2025-02-18 17:15:06 +0400

    feat: add typography

---
+++ packages/ui-core/src/components/typography/lead/lead.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { Lead } from '@/components/typography/lead/lead';
+
+describe('Lead', () => {
+  it('should render default variant', () => {
+    const { asFragment } = render(
+      <Lead>
+        A modal dialog that interrupts the user with important content and expects a response.
+      </Lead>,
+    );
+
+    expect(asFragment()).toMatchInlineSnapshot();
+  });
+});
---
+++ packages/ui-core/src/components/typography/lead/lead.tsx
@@ -0,0 +1,14 @@
+import type { ClassValue } from 'clsx';
+import type { ReactNode } from 'react';
+
+import { cn } from '@/lib/utils';
+
+export function Lead({
+  children,
+  className,
+}: Readonly<{
+  children: ReactNode;
+  className?: ClassValue;
+}>) {
+  return <p className={cn('text-xl text-muted-foreground', className)}>{children}</p>;
+}
